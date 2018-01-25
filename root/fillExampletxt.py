06429bbd8668950a346782a66c6ae0b4ce97f2e3
spiedel <leah@spiedel.co.uk>
2018-01-25 12:40:09 +0000

    testing commiting from correct account

---
+++ root/fillExampletxt.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pickle
+
+#open file
+myfile = open("example.txt", "w")
+
+#does work for larger values but takes a long time to plot
+xsize=500
+ysize=100
+
+#initialise array of random numbers
+array = 20*np.random.rand(xsize,ysize)-10
+
+#talk to others about using pickle?
+#for i in range(0,100):
+#    pickle.dump(array[i], myfile)
+
+#fill human readable file
+for i in range(0,xsize):
+    for j in range (0,ysize):
+        myfile.write('{} '.format(array[i][j]))
+    myfile.write('\n')
+        
+    
+myfile.close()
